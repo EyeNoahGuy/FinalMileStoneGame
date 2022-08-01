af71b7713863a2e2d4354142aadddd292bf220f4
EyeNoahGuy <103152418+EyeNoahGuy@users.noreply.github.com>
2022-08-01 18:48:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,18 +11,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const PLAYERX_WON = 'PLAYERX_WON';
     const PLAYERO_WON = 'PLAYERO_WON';
     const TIE = 'TIE';
-    
-    //Ways To Win
-    const winningConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
+
     
     function handleResultValidation() {
         let roundWon = false;
